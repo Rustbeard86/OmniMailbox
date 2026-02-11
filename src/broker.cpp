bda112f0d1fc804a64f258d651b3cb3c893ced5f
rust86 <nope@nope.mail>
2026-02-10 19:13:30 -0500

    5.2

--- src/broker.cpp
+++ src/broker.cpp
@@ -1,4 +1,23 @@
-// Placeholder for broker implementation
+#include "omni/mailbox_broker.hpp"
+#include "omni/detail/spsc_queue.hpp"
+#include <shared_mutex>
+#include <unordered_map>
+#include <chrono>
+#include <atomic>
+
 namespace omni {
-    // TODO: Implement MailboxBroker
-}
+
+struct MailboxBroker::Impl {
+    struct ChannelState {
+        std::shared_ptr<detail::SPSCQueue> queue;
+        std::string name;
+        std::chrono::steady_clock::time_point created_at;
+    };
+
+    mutable std::shared_mutex registry_mutex_;
+    std::unordered_map<std::string, ChannelState> channels_;
+    std::atomic<size_t> total_created_{0};
+    std::atomic<size_t> total_destroyed_{0};
+};
+
+} // namespace omni
